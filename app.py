adcb8b63cf5a36f1d0e2daeb87e754c86e457e2f
chris <chrisharri2002@hotmail.com>
2022-08-01 00:03:43 +0100

    Read and write logic added to data copier

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
+import os
+from read import get_json_reader
+from write import load_db_table
+import sys
+
+
+# BASE_DIR = '/Users/chris/Documents/Python/research/data/retail_db_json'
+# table_name = 'orders'
+# con = 'postgresql://retail_user:retail_user@localhost:5452/retail_db'
+#
+# file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
+# file_path = f'{BASE_DIR}/{table_name}/{file_name}'
+
+def process_table(BASE_DIR, con, table_name):
+    json_reader = get_json_reader(BASE_DIR, table_name)
+    for df in json_reader:
+        load_db_table(df, con, table_name, df.columns[0])
+
+
 def main():
-    print("Hello World from me")
+    BASE_DIR = os.environ.get('BASE_DIR')
+    table_names = sys.argv[1].split(',')
+    configs = dict(os.environ.items())
+    con = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+    for table_name in table_names:
+        process_table(BASE_DIR, con, table_name)
+
 
 if __name__ == "__main__":
     main()
---
+++ read.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+def get_json_reader(BASE_DIR, table_name, chunksize=1000):
+    file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
+    file_path = f'{BASE_DIR}/{table_name}/{file_name}'
+    return pd.read_json(file_path, lines=True, chunksize=chunksize)
+
+if __name__ == '__main__':
+    BASE_DIR = os.environ.get('BASE_DIR')
+    table_name = os.environ.get('TABLE_NAME')
+    json_reader = get_json_reader(BASE_DIR, table_name)
+    for idx, df in enumerate(json_reader):
+        print(f'Number of records in chunk with index {idx} is {df.shape[0]}')
